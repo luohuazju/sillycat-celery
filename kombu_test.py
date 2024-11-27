8407b564ef408e8ec80136441bb9b5142d90e9c1
Carl <luohuazju@gmail.com>
2024-11-26 23:16:49 -0600

    add debug

--- kombu_test.py
+++ kombu_test.py
@@ -9,5 +9,10 @@ conn = Connection(
 try:
     conn.connect()
     print("Connection successful!")
+    redis_connection = conn.transport.driver_redis()
+    sentinel_connection = redis_connection.connection_pool.connection_kwargs.get('connection_pool', None)
+    if sentinel_connection and hasattr(sentinel_connection, 'sentinel'):
+        master = sentinel_connection.sentinel.master_for('laprocluster').connection_pool.connection_kwargs
+        print(f"Connected to Redis master: {master['host']}:{master['port']}")
 except Exception as e:
     print(f"Connection failed: {e}")
