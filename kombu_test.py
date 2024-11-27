747e6ac7a51839e7fa5dcf2080fce12306b5ab66
Carl <luohuazju@gmail.com>
2024-11-26 23:23:09 -0600

    add debug

--- kombu_test.py
+++ kombu_test.py
@@ -12,14 +12,6 @@ try:
     print("Connection successful!")
     # Extract master name from transport options
     master_name = conn.transport_options.get('master_name')
-    if not master_name:
-        raise ValueError("Master name is not defined in transport options.")
-
-    # Create a Sentinel instance
-    sentinel = Sentinel("sentinel://:laproadminpwd@lapro-base-3:26379/2", socket_timeout=0.5, password='laproadminpwd')
-
-    # Discover the master for the specified master name
-    master_host, master_port = sentinel.discover_master(master_name)
-    print(f"Connected to Redis master: {master_host}:{master_port}")
+    print(master_name)
 except Exception as e:
     print(f"Connection failed: {e}")
