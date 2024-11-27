77615e37abedd63523f29672a73d0d4b652f54b3
Carl <luohuazju@gmail.com>
2024-11-26 22:58:23 -0600

    add debug

--- trigger_task.py
+++ trigger_task.py
@@ -1,8 +1,9 @@
-#from celery_task_ai import ai
-from celery_task_ai import ai_celery
+from celery_task_ai import ai
+#from celery_task_ai import ai_celery
 
 #ai.delay()
-ai_celery.send_task(
-    'ai_task',
-    broker_transport_options={'master_name': 'laprocluster'}
-)
+#ai_celery.send_task(
+#    'ai_task',
+#    broker_transport_options={'master_name': 'laprocluster'}
+#)
+ai.apply_async(kwargs={}, transport_options={'master_name': 'laprocluster'})
