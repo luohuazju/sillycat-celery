c59f874279797f0a2886ca1c886f5ab1ed0f034e
Carl <luohuazju@gmail.com>
2024-11-26 21:41:15 -0600

    check if this load

--- celery_task_ai.py
+++ celery_task_ai.py
@@ -4,7 +4,11 @@ import settings
 
 
 ai_celery = Celery("ai_celery", backend=settings.CELERY_BACKEND, broker=settings.CELERY_BROKER)
-
+ai_celery.conf.update(
+    broker_url=settings.CELERY_BROKER,  # Redis broker URL
+    result_backend=settings.CELERY_BACKEND,  # Redis result backend
+    broker_transport_options=settings.CELERY_BROKER_TRANSPORT_OPTIONS
+)
 
 @ai_celery.task(
     name="ai_task",
