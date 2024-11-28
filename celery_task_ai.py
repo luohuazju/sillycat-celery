dd79e79b572eacfd387925303854ed4e6aeb54b0
Carl <luohuazju@gmail.com>
2024-11-27 21:40:41 -0600

    clean up the codes

--- celery_task_ai.py
+++ celery_task_ai.py
@@ -11,23 +11,23 @@ ai_celery = Celery(
     result_backend_transport_options={'master_name': 'laprocluster'}
 )
 
-ai_celery.conf.update(
-    broker_url=settings.CELERY_BROKER,  # Redis broker URL
-    result_backend=settings.CELERY_BACKEND,  # Redis result backend
-    broker_transport_options={
-        'master_name': 'laprocluster',  # Redis master name
-    },
-    result_backend_transport_options={
-        'master_name': 'laprocluster'
-    }
-)
+#ai_celery.conf.update(
+#    broker_url=settings.CELERY_BROKER,  # Redis broker URL
+#    result_backend=settings.CELERY_BACKEND,  # Redis result backend
+#    broker_transport_options={
+#        'master_name': 'laprocluster',  # Redis master name
+#    },
+#    result_backend_transport_options={
+#        'master_name': 'laprocluster'
+#    }
+#)
 
 # Print configuration to verify
-print("Current Celery Configuration:")
-print(f"Broker URL: {ai_celery.conf.broker_url}")
-print(f"Result Backend: {ai_celery.conf.result_backend}")
-print(f"Broker Transport Options: {ai_celery.conf.broker_transport_options}")
-print(f"Backend Transport Options: {ai_celery.conf.result_backend_transport_options}")
+#print("Current Celery Configuration:")
+#print(f"Broker URL: {ai_celery.conf.broker_url}")
+#print(f"Result Backend: {ai_celery.conf.result_backend}")
+#print(f"Broker Transport Options: {ai_celery.conf.broker_transport_options}")
+#print(f"Backend Transport Options: {ai_celery.conf.result_backend_transport_options}")
 
 @ai_celery.task(
     name="ai_task",
@@ -44,6 +44,6 @@ def ai():
 if __name__ == "__main__":
     print("Broker Transport Options trigger:", ai_celery.conf.broker_transport_options)
     print(f"Backend Transport Options: {ai_celery.conf.result_backend_transport_options}")
-    ai.apply_async(kwargs={}, transport_options={'master_name': 'laprocluster'})
-    #result = ai.delay()  # Trigger the task
-    #print(f"Task has been sent. Task ID: {result.id}")
+    #ai.apply_async(kwargs={}, transport_options={'master_name': 'laprocluster'})
+    result = ai.delay()  # Trigger the task
+    print(f"Task has been sent. Task ID: {result.id}")
--- trigger_task.py
+++ trigger_task.py
@@ -1,10 +1,13 @@
 from celery_task_ai import ai
 #from celery_task_ai import ai_celery
 
+
+result = ai.delay()  # Trigger the task
+print(f"Task has been sent. Task ID: {result.id}")
 # ai.delay()
-ai.apply_async(kwargs={},
-               transport_options={'master_name': 'laprocluster'},
-               result_backend_transport_options={'master_name': 'laprocluster'})
+#ai.apply_async(kwargs={},
+#               transport_options={'master_name': 'laprocluster'},
+#               result_backend_transport_options={'master_name': 'laprocluster'})
 #ai_celery.send_task(
 #    'ai_task',
 #    broker_transport_options={'master_name': 'laprocluster'}
