38392b3026d8ed23bd685ede478ed9788f13710d
Carl <luohuazju@gmail.com>
2024-11-30 09:41:13 -0600

    verify the backend mongo

--- celery_task_ai.py
+++ celery_task_ai.py
@@ -8,7 +8,7 @@ ai_celery = Celery(
     backend=settings.CELERY_BACKEND,
     broker=settings.CELERY_BROKER,
     broker_transport_options={'master_name': 'laprocluster'},
-    result_backend_transport_options={'master_name': 'laprocluster'}
+    # result_backend_transport_options={'master_name': 'laprocluster'}
 )
 
 @ai_celery.task(
@@ -25,6 +25,6 @@ def ai():
 # Trigger the task conditionally
 if __name__ == "__main__":
     print("Broker Transport Options trigger:", ai_celery.conf.broker_transport_options)
-    print(f"Backend Transport Options: {ai_celery.conf.result_backend_transport_options}")
+    # print(f"Backend Transport Options: {ai_celery.conf.result_backend_transport_options}")
     result = ai.delay()  # Trigger the task
     print(f"Task has been sent. Task ID: {result.id}")
--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py
 broker_url = 'sentinel://:laproadminpwd@lapro-base-3:26379/2'
 broker_transport_options = {'master_name': 'laprocluster'}
-result_backend_transport_options={'master_name': 'laprocluster'}
+# result_backend_transport_options={'master_name': 'laprocluster'}
